be7bee675fab6549d048840431bcbb61266330c0
cuong <lietthoaiphucuong@gmail.com>
2023-03-15 16:18:35 +0700

    fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,18 @@
-// 1. Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
-// 2. Define a schema for each collection you'd like to validate.
-const blog = defineCollection({
+
+const BlogPosts = defineCollection({
   schema: z.object({
-    
-    title: z.string().max(60,"For optimize SEO, Vui long nhập 60 ký tự hoặc nhỏ hơn"),
-    description:z.string().max(160,"For optimize SEO, Vui long nhập 160 ký tự hoặc nhỏ hơn"),
-    date:z.date(),
+    title: z.string(),
+    excerpt: z.string(),
+    category: z.string().trim(),
+    author: z.string().trim(),
     draft: z.boolean().optional(),
-    author: z.enum(["hieu1","hieu2","hieu3"]),
-    category: z.enum(["CSS","HTML"]),
-    image: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    publishDate: z.string().transform((str) => new Date(str)),
   }),
 });
-// 3. Export a single `collections` object to register your collection(s)
+
 export const collections = {
-  blog,
+  blog: BlogPosts,
 };
--- src/utils/all.js
+++ src/utils/all.js
@@ -1,4 +1,7 @@
-/** */
+import getReadingTime from "reading-time";
+import { toString } from "mdast-util-to-string";
+
+/** Format Date */
 export const getFormattedDate = (date) =>
   date
     ? new Date(date).toLocaleDateString("en-us", {
@@ -7,3 +10,23 @@ export const getFormattedDate = (date) =>
         day: "numeric",
       })
     : "";
+
+/** Estimated Reading time */
+export function remarkReadingTime() {
+  return function (tree, { data }) {
+    const textOnPage = toString(tree);
+    const readingTime = getReadingTime(textOnPage);
+
+    data.astro.frontmatter.estReadingTime = readingTime.minutes;
+  };
+}
+
+/** Check if an Image Path is Relative or Absolute */
+export const checkImageUrl = (image, url) => {
+  try {
+    new URL(image);
+    return image;
+  } catch (error) {
+    return new URL(image, url).toString();
+  }
+};
